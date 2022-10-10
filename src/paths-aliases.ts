81c09674d3e852b20745a1d35f5d321db24ead51
DlopezS98 <01dlopezs98@gmail.com>
2022-10-09 21:17:35 -0600

    new paths shortcuts

--- src/paths-aliases.ts
+++ src/paths-aliases.ts
@@ -7,4 +7,7 @@ addAliases({
   '@Shared': join(__dirname, 'shared'),
   '@Config': join(__dirname, 'config'),
   '@Database': join(__dirname, 'infrastructure', 'database'),
+  '@Interfaces': join(__dirname, 'core', 'interfaces'),
+  '@Services': join(__dirname, 'core', 'services'),
+  '@Repositories': join(__dirname, 'infrastruture', 'repositories'),
 });
